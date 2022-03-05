591b3b59063cf1623006572b548e05d448ffb8ea
Reynaldi <reynaldi@gitlab.lab>
2022-03-06 00:28:36 +0700

    second commit

---
+++ js/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Hoodie White',
+        productNumber: '999',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
